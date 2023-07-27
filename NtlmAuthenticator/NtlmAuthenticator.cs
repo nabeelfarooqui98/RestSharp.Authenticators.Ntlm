3a54f18692b11f812f32846cee083d57042c1e9c
Nabeel Farooqui <nabeelfarooqui98@yahoo.com>
2023-07-27 12:16:51 +0500

    Add project and solution

---
+++ NtlmAuthenticator/NtlmAuthenticator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RestSharp.Authenticators.Ntlm 
+{
+	public class NtlmAuthenticator : IAuthenticator
+	{
+        public NtlmAuthenticator(string username, string password, string domain = "")
+        {
+            
+        }
+        public ValueTask Authenticate(RestClient client, RestRequest request)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
